768d46fa35764dfe7d279c1caf666517c6fb7611
Daniel Newman <danielnewman09@gmail.com>
2018-12-30 22:48:27 -0500

    adding latex equation builder

--- project/__init__.py
+++ project/__init__.py
@@ -18,7 +18,9 @@ db = SQLAlchemy(app)
 
 from project.users.views import users_blueprint
 from project.home.views import home_blueprint
+from project.latex.views import latex_blueprint
 
 # register our blueprints
 app.register_blueprint(users_blueprint)
 app.register_blueprint(home_blueprint)
+app.register_blueprint(latex_blueprint)
---
+++ project/latex/views.py
@@ -0,0 +1,90 @@
+#################
+#### imports ####
+#################
+
+from flask import flash, redirect, render_template, request, \
+    session, url_for, Blueprint
+from functools import wraps
+from secrets import token_hex
+import os
+from subprocess import call
+
+import cairosvg
+
+
+################
+#### config ####
+################
+
+latex_blueprint = Blueprint(
+    'latex', __name__,
+    template_folder='templates'
+)
+
+################
+#### routes ####
+################
+
+# route for handling the login page logic
+@latex_blueprint.route('/latex', methods=['GET', 'POST'])
+def latex():
+    tmpfile = token_hex(16)
+    tmpfile = 'tmp/' + tmpfile
+
+    print(os.path.dirname(os.path.abspath(__file__)))
+    imgUrl = 'test.png'
+
+    if not os.path.exists(tmpfile):
+        os.makedirs(tmpfile)
+
+    latexPreambleString = \
+    r'\\documentclass[preview,border={0pt 5pt 0pt 0pt}]{standalone}\n\
+\\usepackage{fancyhdr}\n\
+\\usepackage{graphicx}\n\
+\\usepackage{amssymb}\n\
+\\usepackage{epstopdf}\n\
+\\usepackage{amsmath}\n\
+\\usepackage{amssymb}\n\
+\\usepackage{cite}\n\
+\\usepackage{float}\n\
+\\usepackage[normalem]{ulem}\n\
+\\usepackage{multirow}\n\
+\\usepackage{booktabs}\n\
+\\usepackage{wrapfig}\n\
+\\usepackage{caption}\n\
+\\usepackage{tikz}\n\
+\\begin{document}'
+    
+    SVGText = ''
+    latexEquation = ''
+
+    if request.method == 'POST':
+
+        submittedPreamble = "%r"%request.form['latexPreamble']
+        submittedPreamble = submittedPreamble.replace("'\\","").replace("'","")
+        submittedEquation = "%r"%request.form['latexEquation']
+        latexEquation = request.form['latexEquation']
+        submittedEquation = submittedEquation.replace("\\\\","\\").replace("'","")
+        texColor = request.form['texColor']
+        file = open('{}/test.tex'.format(tmpfile),'w')
+        for i in range(len(submittedPreamble.split('\\r\\n\\'))):
+            file.write(submittedPreamble.split('\\r\\n\\')[i])
+            file.write('\n')
+        for j in range(len(submittedEquation.split('\\r'))):
+            file.write(submittedEquation.split('\\r\\n')[j])
+            file.write('\n')
+        file.write(r'\end{document}')
+        file.close()
+
+        call(['pdflatex', '-output-directory', tmpfile, '{}/test.tex'.format(tmpfile)])
+        call(['pdfcrop','{}/test.pdf'.format(tmpfile),'{}/test.pdf'.format(tmpfile)])
+        call(['pdf2svg','{}/test.pdf'.format(tmpfile),'{}/test.svg'.format(tmpfile)])
+        # cairosvg.svg2png(url='{}/test.svg'.format(tmpfile), write_to='{}/test.png'.format(tmpfile),dpi=int(300))
+
+        with open('{}/test.svg'.format(tmpfile), 'r') as myfile:
+            SVGText = myfile.read().replace('\n', '')
+            SVGText = SVGText.split('<defs>')[1]
+            SVGText = SVGText.replace('rgb(0%,0%,0%)',texColor)
+
+    error = None
+    return render_template('latex.html', latexEquation=latexEquation,SVGText=SVGText, latexPreamble=latexPreambleString.replace('\\\\',"\\").replace('\\n\\',''))
